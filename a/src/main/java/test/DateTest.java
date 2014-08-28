da365d7065ade11c66851e3abf91287d4160f363
wangshubao <wangshubao@lashou-inc.com>
2014-08-28 18:03:45 +0800

    123test

---
+++ a/src/main/java/test/DateTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		String date = "2014-01-02";
+		Date date1 =new Date(date);
+		System.out.println(date1);
+	}
+
+}
